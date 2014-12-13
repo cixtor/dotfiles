5e4756ba307832a27514037363863a9fe62c99e4
Cixtor <cixtords@gmail.com>
2014-12-13 09:03:32 -0500

    Modified. Plugin installer for main text editor

--- .config/sublime-text-3/Plugins.sh
+++ .config/sublime-text-3/Plugins.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 inst_path="$HOME/.config/sublime-text-3/Packages/"
 repositories=(
-    "skuroda/Sublime-AdvancedNewFile AdvancedNewFile"
-    "wbond/sublime_alignment Alignment"
-    "spadgos/sublime-jsdocs DocBlockr"
-    "Medalink/laravel-blade LaravelBlade"
-    "aziz/PlainTasks PlainTasks"
-    "aziz/knockdown knockdown"
-    "technosophos/Vala-TMBundle Vala"
-    "jashkenas/coffee-script-tmbundle CoffeeScript"
+    "Vala|technosophos/Vala-TMBundle"
+    "DocBlockr|spadgos/sublime-jsdocs"
+    "AdvancedNewFile|skuroda/Sublime-AdvancedNewFile"
+    "Alignment|wbond/sublime_alignment"
+    "PlainTasks|aziz/PlainTasks"
+    "knockdown|aziz/knockdown"
+    "CoffeeScript|jashkenas/coffee-script-tmbundle"
+    "LaravelBlade|Medalink/laravel-blade"
 )
 if [ -e "${inst_path}" ]; then
     cd $inst_path
     for repo in "${repositories[@]}"; do
-        repo_name=$(echo $repo | awk '{print $1}')
+        local_name=$(echo $repo | awk -F '|' '{print $1}')
+        repo_name=$(echo $repo | awk -F '|' '{print $2}')
         repo_url="https://github.com/${repo_name}"
-        local_name=$(echo $repo | awk '{print $2}')
         echo -e "Install '\e[0;33m${repo_name}\e[0m' as '\e[0;32m${local_name}\e[0m'"
         git clone "${repo_url}" "${local_name}"
         echo
